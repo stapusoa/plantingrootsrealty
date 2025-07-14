c657a020f3883f3b07202317a3e50cc65151aa82
Sara Tapusoa <79812543+stapusoa@users.noreply.github.com>
2025-07-14 17:06:29 -0600

    remove instances of import.meta

--- apps/cms/src/env.ts
+++ apps/cms/src/env.ts
@@ -1,4 +1,4 @@
-export const GITHUB_TOKEN = import.meta.env?.VITE_GITHUB_TOKEN || '';
+export const GITHUB_TOKEN = process.env.VITE_GITHUB_TOKEN || '';
 export const REPO_OWNER = "stapusoa";
 export const REPO_NAME = "plantingrootsrealty-cms";
 export const CONTENT_PATH = "content/pages";
