c9f025bbf4e9f301bdca4f89ba1063ed95ecb98e
Sara Tapusoa <79812543+stapusoa@users.noreply.github.com>
2025-08-26 17:05:08 -0600

    feat(home): prr-52 update content and styling on home page

--- apps/web/src/pages/Home/ContactFormOld.tsx
+++ apps/web/src/pages/Home/ContactFormOld.tsx
@@ -1,12 +1,13 @@
 export function ContactFormOld() {
   return (
     <div className="flex relative w-full bg-white py-16 px-4 md:px-12">
-      <div className="max-w-12xl mx-auto">
-        <div className="text-center mb-10">
-          <h2 className="text-4xl font-bold font-merriweather">Contact Form</h2>
+      <div className="max-w-6xl w-full mx-auto grid grid-cols-1">
+        <div className="grid-cols-1 text-center mb-10">
+          <h2 className="text-4xl font-bold font-merriweather">Love to hear from you,<br/>Get in touch</h2>
           <hr className="mt-4 w-16 border-t-4 border-gray-300 mx-auto" />
         </div>
         <form className="space-y-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <div>
             <label htmlFor="name" className="block text-sm font-medium text-gray-700">
               Name
@@ -35,6 +36,8 @@ export function ContactFormOld() {
               className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm px-4 py-2 focus:ring-primary focus:border-primary"
             />
           </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <div>
             <label htmlFor="email" className="block text-sm font-medium text-gray-700">
               Email
@@ -47,6 +50,20 @@ export function ContactFormOld() {
               placeholder="email@gmail.com"
               className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm px-4 py-2 focus:ring-primary focus:border-primary"
             />
+          </div>
+          <div>
+            <label htmlFor="dropdown" className="block text-sm font-medium text-gray-700">
+              Budget
+            </label>
+            <input
+              id="dropdown"
+              type="dropdown"
+              name="dropdown"
+              required
+              placeholder="$0"
+              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm px-4 py-2 focus:ring-primary focus:border-primary"
+            />
+          </div>
           </div>
           <div>
             <label htmlFor="message" className="block text-sm font-medium text-gray-700">
